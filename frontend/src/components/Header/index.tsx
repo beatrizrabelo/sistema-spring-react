6b48ad277406bda1ff32f71fb2ce5b0db521ed79
Beatriz Rabelo <beatriz.devrabelo@gmail.com>
2022-09-11 20:46:16 -0300

    Adicionando outros componentes

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="Logo" />
+                <h1>Projeto</h1>
+                <p>
+                    Desenvolvendo para estudos.
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
